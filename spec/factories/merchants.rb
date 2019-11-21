5cde931dd0cc116116b728a4da890aa8952ef96c
Scott Regenthal <scottregenthal@gmail.com>
2019-11-20 20:09:25 -0700

    Add merchant API index spec, factory name, route and controller

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :merchant do
-    name { "MyString" }
-    created_at { "2019-11-19 16:04:19" }
-    updated_at { "2019-11-19 16:04:19" }
+    name {Faker::Company.name}
   end
 end
