6b9762b2b19aba2b9159383c13b2fa713aacdf5e
Scott Regenthal <scottregenthal@gmail.com>
2019-11-20 21:01:22 -0700

    Add merchant API show spec, route and controller action

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe 'Merchants API' do
 
     get '/api/v1/merchants'
 
-    expect(response).to be_successful
-
     merchants = JSON.parse(response.body)
 
+    expect(response).to be_successful
     expect(merchants['data'].length).to eq(3)
+  end
+
+  it 'returns show record by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant['data']['id'].to_i).to eq(id)
   end
 end
