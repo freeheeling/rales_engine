1822f1dcc2fa74e9fb181c4772416d1678d34074
Scott Regenthal <scottregenthal@gmail.com>
2019-11-21 15:36:18 -0700

    Add merchant param tests, query controller and route based on request param

---
+++ app/controllers/api/v1/merchants/query_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Merchants::QueryController < ApplicationController
+  def show
+    merchant = Merchant.find_by(query_params)
+    serialized_merchant = MerchantSerializer.new(merchant)
+    render json: serialized_merchant
+  end
+
+  private
+    def query_params
+      params.permit(:id, :name, :created_at, :updated_at)
+    end
+end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Merchants API' do
+RSpec.describe 'Merchant API endpoints' do
   it 'returns index of merchant records' do
     create_list(:merchant, 3)
 
@@ -21,5 +21,53 @@ RSpec.describe 'Merchants API' do
 
     expect(response).to be_successful
     expect(merchant['data']['id'].to_i).to eq(id)
+  end
+
+  it 'finds record based on merchant id param' do
+    create_list(:merchant, 3)
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?id=#{merchant.id}"
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json_merchant['data']['id'].to_i).to eq(merchant.id)
+  end
+
+  it 'finds record based on merchant name param' do
+    create_list(:merchant, 3)
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?name=#{merchant.name}"
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json_merchant['data']['attributes']['name']).to eq(merchant.name)
+  end
+
+  it 'finds record based on creation timestamp' do
+    create_list(:merchant, 3)
+    merchant = create(:merchant, created_at: '2012-03-27 14:53:59')
+
+    get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json_merchant['data']['id'].to_i).to eq(merchant.id)
+  end
+
+  it 'finds record based on update timestamp' do
+    create_list(:merchant, 3)
+    merchant = create(:merchant, updated_at: '2012-03-27 14:53:59')
+
+    get "/api/v1/merchants/find?updated_at=#{merchant.updated_at}"
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json_merchant['data']['id'].to_i).to eq(merchant.id)
   end
 end
