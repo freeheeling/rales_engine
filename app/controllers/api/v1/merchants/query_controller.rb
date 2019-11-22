2d7ded40dc545c39ace0bcb3fcb05d3201e9aefc
Scott Regenthal <scottregenthal@gmail.com>
2019-11-21 20:49:40 -0700

    Create index route and action for QueryController and show route and action for RandomController

--- app/controllers/api/v1/merchants/query_controller.rb
+++ app/controllers/api/v1/merchants/query_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::Merchants::QueryController < ApplicationController
     render json: serialized_merchant
   end
 
+  def index
+    merchants = Merchant.where(query_params)
+    serialized_merchants = MerchantSerializer.new(merchants)
+    render json: serialized_merchants
+  end
+
   private
     def query_params
       params.permit(:id, :name, :created_at, :updated_at)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/find', to: 'query#show'
+        get '/find_all', to: 'query#index'
+        get '/random', to: 'random#show'
       end
       resources :merchants, only: [:index, :show]
     end
