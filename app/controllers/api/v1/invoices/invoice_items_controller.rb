94bb20ac80f9a66e133fa81ca6b42e6dac12513e
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Edit serializer name and file from Invoice Items to Invoice Item

--- app/controllers/api/v1/invoices/invoice_items_controller.rb
+++ app/controllers/api/v1/invoices/invoice_items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Invoices::InvoiceItemsController < ApplicationController
   def index
     invoice = Invoice.find_by(id: params[:id])
-    serialized_invoice_items = InvoiceItemsSerializer.new(invoice.invoice_items)
+    serialized_invoice_items = InvoiceItemSerializer.new(invoice.invoice_items)
     render json: serialized_invoice_items
   end
 end
--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,4 @@
-class InvoiceItemsSerializer
+class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :quantity, :unit_price, :item_id, :invoice_id
 end
