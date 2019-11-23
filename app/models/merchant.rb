ae5ee6a581e5259f353cee2fbc2a45e68f24258d
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Add model method for aggreagating all merchants' revenue on a given date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,10 +8,18 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.rank_by_revenue(quantity)
-    joins(:invoice_items, :transactions)
-      .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
-      .group(:id)
-      .order('revenue DESC')
-      .limit(quantity)
+    joins(:invoice_items, :transactions).
+      select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').
+      group(:id).
+      order('revenue DESC').
+      limit(quantity)
+  end
+
+  def self.revenue_on_date(date)
+    Invoice.joins(:invoice_items, :transactions).
+      merge(Transaction.successful).
+      where("date(invoices.created_at) = date('#{date}')").
+      sum('invoice_items.quantity * invoice_items.unit_price').
+      to_s
   end
 end
