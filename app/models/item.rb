2ffb50ca46f3b44a11a06ae613cf13b18750ab5d
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Create model test and add class method to item model that ranks items by sales revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  
+
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
 
   validates_presence_of :name, :description, :unit_price
+
+  def self.rank_by_revenue(quantity)
+    joins(:transactions).
+      select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').
+      merge(Transaction.successful).
+      group(:id).
+      order('revenue DESC').
+      limit(quantity)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it {should belong_to :merchant}
-    
+
     it {should have_many :invoice_items}
     it {should have_many(:invoices).through(:invoice_items)}
     it {should have_many(:transactions).through(:invoices)}
@@ -13,5 +13,32 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
     it {should validate_presence_of :unit_price}
+  end
+
+  describe 'class methods' do
+    it 'rank by revenue' do
+      item = create(:item)
+      item2 = create(:item)
+      invoice = create(:invoice)
+      invoice2 = create(:invoice)
+      create(
+        :invoice_item,
+        quantity: '1',
+        unit_price: '1.00',
+        item: item,
+        invoice: invoice
+      )
+      create(
+        :invoice_item,
+        quantity: '2',
+        unit_price: '2.00',
+        item: item2,
+        invoice: invoice2
+      )
+      create(:transaction, invoice: invoice)
+      create(:transaction, invoice: invoice2)
+
+      expect(Item.rank_by_revenue(2)).to eq([item2, item])
+    end
   end
 end
