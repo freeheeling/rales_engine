75e7e628817dce42721889263646df4d1db8c302
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Convert invoice item unit price attribute to a string in serializer

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,6 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :quantity, :unit_price, :item_id, :invoice_id
+
+  attributes :id, :quantity, :item_id, :invoice_id
+  attribute :unit_price {|item| item.unit_price.to_s}
 end
