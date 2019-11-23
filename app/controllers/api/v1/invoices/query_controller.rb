7d70cee8b66aab3c7a421602f35fb23f65b4297c
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Add multi-find request invoice param tests, namespaced route and index action within query controller

--- app/controllers/api/v1/invoices/query_controller.rb
+++ app/controllers/api/v1/invoices/query_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::Invoices::QueryController < ApplicationController
     render json: serialized_invoice
   end
 
+  def index
+    invoices = Invoice.where(query_params)
+    serialized_invoices = InvoiceSerializer.new(invoices)
+    render json: serialized_invoices
+  end
+
   private
     def query_params
       params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
--- spec/requests/api/v1/invoices/record_endpts_spec.rb
+++ spec/requests/api/v1/invoices/record_endpts_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe 'Invoice API record endpoints' do
   before :each do
     create_list(:invoice, 3)
 
+    @customer = create(:customer)
+    @merchant = create(:merchant)
     @invoice = create(
       :invoice,
+      customer: @customer,
+      merchant: @merchant,
       status: 'shipped',
       created_at: '2014-03-27 14:53:59',
       updated_at: '2014-03-27 14:53:59'
@@ -41,7 +45,7 @@ RSpec.describe 'Invoice API record endpoints' do
     expect(invoice[:data][:id].to_i).to eq(@invoice.id)
   end
 
-  it 'finds record based on invoice status' do
+  it 'finds record based on invoice status query' do
     get "/api/v1/invoices/find?status=#{@invoice.status}"
 
     invoice = JSON.parse(response.body, symbolize_names: true)
@@ -50,7 +54,7 @@ RSpec.describe 'Invoice API record endpoints' do
     expect(invoice[:data][:id].to_i).to eq(Invoice.ids.first)
   end
 
-  it 'finds record based on customer id' do
+  it 'finds record based on customer id query' do
     get "/api/v1/invoices/find?customer_id=#{@invoice.customer_id}"
 
     invoice = JSON.parse(response.body, symbolize_names: true)
@@ -59,7 +63,7 @@ RSpec.describe 'Invoice API record endpoints' do
     expect(invoice[:data][:id].to_i).to eq(@invoice.id)
   end
 
-  it 'finds record based on merchant id' do
+  it 'finds record based on merchant id query' do
     get "/api/v1/invoices/find?merchant_id=#{@invoice.merchant_id}"
 
     invoice = JSON.parse(response.body, symbolize_names: true)
@@ -68,7 +72,7 @@ RSpec.describe 'Invoice API record endpoints' do
     expect(invoice[:data][:id].to_i).to eq(@invoice.id)
   end
 
-  it 'finds record based on creation timestamp' do
+  it 'finds record based on creation timestamp query' do
     get "/api/v1/invoices/find?created_at=#{@invoice.created_at}"
 
     invoice = JSON.parse(response.body , symbolize_names: true)
@@ -77,12 +81,80 @@ RSpec.describe 'Invoice API record endpoints' do
     expect(invoice[:data][:id].to_i).to eq(@invoice.id)
   end
 
-  it 'finds record based on update timestamp' do
+  it 'finds record based on update timestamp query' do
     get "/api/v1/invoices/find?updated_at=#{@invoice.updated_at}"
 
     invoice = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
     expect(invoice[:data][:id].to_i).to eq(@invoice.id)
+  end
+
+  it 'finds all records based on invoice id query' do
+    get "/api/v1/invoices/find_all?id=#{@invoice.id}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(1)
+    expect(invoices[:data][0][:id].to_i).to eq(@invoice.id)
+  end
+
+  it 'finds all records based on invoice status query' do
+    get "/api/v1/invoices/find_all?status=#{@invoice.status}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(4)
+    expect(invoices[:data][0][:attributes][:status]).to eq(@invoice.status)
+  end
+
+  it 'finds all records based on customer id query' do
+    invoice2 = create(:invoice, customer: @customer)
+
+    get "/api/v1/invoices/find_all?customer_id=#{@customer.id}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(2)
+    expect(invoices[:data][0][:attributes][:id].to_i).to eq(@invoice.id)
+  end
+
+  it 'finds all records based on merchant id query' do
+    invoice2 = create(:invoice, merchant: @merchant)
+
+    get "/api/v1/invoices/find_all?merchant_id=#{@merchant.id}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(2)
+    expect(invoices[:data][0][:attributes][:id].to_i).to eq(@invoice.id)
+  end
+
+  it 'finds all records based on creation timestamp query' do
+    invoice2 = create(:invoice, created_at: '2014-03-27 14:53:59')
+
+    get "/api/v1/invoices/find_all?created_at=#{@invoice.created_at}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(2)
+    expect(invoices[:data][0][:id].to_i).to eq(@invoice.id)
+  end
+
+  it 'finds all records based on update timestamp query' do
+    invoice2 = create(:invoice, updated_at: '2014-03-27 14:53:59')
+
+    get "/api/v1/invoices/find_all?updated_at=#{@invoice.updated_at}"
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(invoices[:data].length).to eq(2)
+    expect(invoices[:data][0][:id].to_i).to eq(@invoice.id)
   end
 end
