931f232a7adab7c36f1c090b8c1ec8830cfa9863
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Add request and model tests for aggregating revenue on given date

--- spec/requests/api/v1/merchants/business_logic_spec.rb
+++ spec/requests/api/v1/merchants/business_logic_spec.rb
@@ -5,38 +5,36 @@ RSpec.describe 'Merchant API business logic endpoints' do
     customer = create(:customer)
     @merchant = create(:merchant)
     @merchant2 = create(:merchant)
-    invoice = create(:invoice, customer: customer, merchant: @merchant)
-    invoice2 = create(:invoice, customer: customer, merchant: @merchant2)
-    item = create(:item, merchant: @merchant)
-    item2 = create(:item, merchant: @merchant2)
-    @invoice_item = create(
-      :invoice_item,
-      quantity: '1',
-      unit_price: '1.00',
-      invoice: invoice,
-      item: item,
-      created_at: '2014-03-27 14:53:59'
-    )
-    invoice_item2 = create(
-      :invoice_item,
-      quantity: '1',
-      unit_price: '2.00',
-      invoice: invoice2,
-      item: item2,
-      created_at: @invoice_item.created_at
-    )
-    transaction = create(:transaction, result: 'success', invoice: invoice)
-    transaction2 = create(:transaction, result: 'success', invoice: invoice2)
+    @merchant3 = create(:merchant)
+    @invoice = create(:invoice, merchant: @merchant, created_at: '2014-03-27 14:53:59')
+    invoice2 = create(:invoice, merchant: @merchant2, created_at: @invoice.created_at)
+    invoice3 = create(:invoice, merchant: @merchant3, created_at: @invoice.created_at)
+    create(:invoice_item, quantity: '1', unit_price: '1.00', invoice: @invoice)
+    create(:invoice_item, quantity: '2', unit_price: '2.00', invoice: invoice2)
+    create(:invoice_item, quantity: '1', unit_price: '2.00', invoice: invoice3)
+    create(:transaction, result: 'success', invoice: @invoice)
+    create(:transaction, result: 'success', invoice: invoice2)
+    create(:transaction, result: 'success', invoice: invoice3)
   end
 
   it 'returns variable number of merchants ranked by total revenue' do
-    get '/api/v1/merchants/most_revenue?quantity=2'
+    get '/api/v1/merchants/most_revenue?quantity=3'
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(merchants[:data].length).to eq(2)
+    expect(merchants[:data].length).to eq(3)
     expect(merchants[:data][0][:id].to_i).to eq(@merchant2.id)
-    expect(merchants[:data][1][:id].to_i).to eq(@merchant.id)
+    expect(merchants[:data][1][:id].to_i).to eq(@merchant3.id)
+    expect(merchants[:data][2][:id].to_i).to eq(@merchant.id)
+  end
+
+  it 'returns cumulative revenue across all merchants for a given date' do
+    get "/api/v1/merchants/revenue?date=#{@invoice.created_at}"
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchants[:data][:attributes][:total_revenue]).to eq('7.0')
   end
 end
