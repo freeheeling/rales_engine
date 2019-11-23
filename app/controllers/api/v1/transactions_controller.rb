cc4c74843e19f4cb46f7f20f96a9d3cb0ea214f1
Scott Regenthal <scottregenthal@gmail.com>
2019-11-23 13:26:04 -0700

    Add transaction show spec, route and controller action

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::TransactionsController < ApplicationController
     serialized_transactions = TransactionSerializer.new(transactions)
     render json: serialized_transactions
   end
+
+  def show
+    transaction = Transaction.find_by(id: params[:id])
+    serialized_transaction = TransactionSerializer.new(transaction)
+    render json: serialized_transaction
+  end
 end
--- spec/requests/api/v1/transactions/record_endpts_spec.rb
+++ spec/requests/api/v1/transactions/record_endpts_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Transaction API record endpoints' do
     expect(response).to be_successful
     expect(transactions[:data].length).to eq(3)
   end
+
+  it 'returns show record by transaction id' do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/#{id}"
+
+    transaction = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(transaction[:data][:id].to_i).to eq(id)
+  end
 end
