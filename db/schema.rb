0eb7e7098c65aed96dcc533e5a51703fd41a5cc9
Scott Regenthal <scottregenthal@gmail.com>
2019-12-02 07:33:43 -0700

    Change data type to case insensitive

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,12 @@
 ActiveRecord::Schema.define(version: 2019_11_19_230419) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.citext "first_name"
+    t.citext "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_230419) do
   create_table "invoices", force: :cascade do |t|
     t.bigint "customer_id"
     t.bigint "merchant_id"
-    t.string "status"
+    t.citext "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
@@ -44,7 +45,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_230419) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.text "description"
     t.float "unit_price"
     t.bigint "merchant_id"
@@ -54,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_230419) do
   end
 
   create_table "merchants", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -63,7 +64,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_230419) do
     t.bigint "invoice_id"
     t.string "credit_card_number"
     t.string "credit_card_expiration_date"
-    t.string "result"
+    t.citext "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
